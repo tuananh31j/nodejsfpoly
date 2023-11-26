79e108c2e78c2f043ada19d3389b61bebf32cee8
Nguyen Tuan Anh <tuananh31j@gmail.com>
2023-11-26 20:58:18 +0700

    chore: remove .env in .gitignore

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -7,17 +7,17 @@ const express = require('express')
 const router = express.Router();
 const middlewareCheckToken = require('../../app/middlewares/CheckToken');
 
-router.use('/products',middlewareCheckToken.verifyUser, middlewareCheckToken.verifyAdmin, routerProduct);
-router.use('/banners',middlewareCheckToken.verifyUser, middlewareCheckToken.verifyAdmin, routerBanner);
-router.use('/categories',middlewareCheckToken.verifyUser, middlewareCheckToken.verifyAdmin, routerCategory);
-router.use('/customers',middlewareCheckToken.verifyUser, middlewareCheckToken.verifyAdmin, routerCustomer);
+// router.use('/products',middlewareCheckToken.verifyUser, middlewareCheckToken.verifyAdmin, routerProduct);
+// router.use('/banners',middlewareCheckToken.verifyUser, middlewareCheckToken.verifyAdmin, routerBanner);
+// router.use('/categories',middlewareCheckToken.verifyUser, middlewareCheckToken.verifyAdmin, routerCategory);
+// router.use('/customers',middlewareCheckToken.verifyUser, middlewareCheckToken.verifyAdmin, routerCustomer);
 
 
 router.use('/auth', routerAuth);
 
-// router.use('/products', routerProduct);
-// router.use('/banners', routerBanner);
-// router.use('/categorys', routerCategory);
-// router.use('/customers', routerCustomer);
+router.use('/products', routerProduct);
+router.use('/banners', routerBanner);
+router.use('/categorys', routerCategory);
+router.use('/customers', routerCustomer);
 
 module.exports = router;
