b977a89cb60d8bff406b9e258594b712dd7e90f9
Nguyen Tuan Anh <tuananh31j@gmail.com>
2023-11-29 00:36:46 +0700

    chore: vercel

--- src/utilities/mongoose.js
+++
@@ -1,9 +0,0 @@
-const multipleMongooseToObject = (mongoose) => {
-    return mongoose.map(item => item.toObject())
-}
-
-const mongooseToObject = (mongoose) => {
-    return mongoose ? mongoose.toObject : mongoose
-}
-
-module.exports = { multipleMongooseToObject, mongooseToObject }
