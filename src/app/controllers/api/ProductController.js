a98927813c1a766e0b5994a84549c30047e6d13e
Nguyen Tuan Anh <tuananh31j@gmail.com>
2023-11-30 08:55:53 +0700

    update

--- src/app/controllers/api/ProductController.js
+++ src/app/controllers/api/ProductController.js
@@ -1,4 +1,4 @@
-const Product = require('../../../../models/Product.js')
+const Product = require('../../../models/Product.js')
 const isValidationPro = require('../../../validations/Product')
 class ProductController {
     getAll(rep, res, next) {
