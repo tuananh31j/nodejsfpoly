4b7bf39437911c8f46ed9a91d1a12bec719e8ae8
Nguyen Tuan Anh <tuananh31j@gmail.com>
2023-11-30 10:50:59 +0700

    99%

--- src/app/validations/Customer.js
+++ src/app/validations/Customer.js
@@ -1,10 +1,13 @@
 const Joi = require('joi')
+require('dotenv').config();
 
 const isValidationUser = Joi.object({
     email: Joi.string().email().required(),
     password: Joi.string().required(),
     phone: Joi.string().required().max(10),
-    name: Joi.string().required()
+    name: Joi.string().required(),
+    img: Joi.string().default(process.env.AVT_DEFAULT),
+    role: Joi.number().default(0).min(0).max(1)
 })
 
 module.exports = isValidationUser;
